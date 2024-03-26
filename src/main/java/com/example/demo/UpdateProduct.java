fa06467b3ea23afea9168ce7f2a520767de51b18
Admin <hai55jmt@gmail.com>
2024-03-26 23:56:57 +0700

    #6 -fix code

--- src/main/java/com/example/demo/UpdateProduct.java
+++ src/main/java/com/example/demo/UpdateProduct.java
@@ -3,5 +3,6 @@ package com.example.demo;
 public class UpdateProduct {
     public static void main(String[] args) {
         System.out.println("Update product");
+        System.out.println("Hot fix");
     }
 }
